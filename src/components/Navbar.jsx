3de47900702239923c56903caea6799e086938df
ahmedfahim21 <ahmed.fahim0207@gmail.com>
2023-01-01 16:37:24 +0530

    added Navbar and Hero

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+import { useState } from 'react'
+
+function Navbar() {
+    const [nav, setNav]=useState(true);
+
+    const handleNav = () =>{
+        setNav(!nav)
+    }
+
+  return (
+    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
+      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT</h1>
+      <ul className='hidden md:flex'>
+        <li className='p-4'>HOME</li>
+        <li className='p-4'>COMPANY</li>
+        <li className='p-4'>RESOURCES</li>
+        <li className='p-4'>ABOUT</li>
+        <li className='p-4'>CONTACT</li>
+      </ul>
+      <div onClick={handleNav} className='block md:hidden'>
+        {
+            !nav? <AiOutlineClose size={20} /> :<AiOutlineMenu size={20}/>
+        }
+        </div>
+      <div className={!nav? 'fixed left-0 top-0 w-[60%] border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
+      <h1 className='w-full text-3xl font-bold m-7 text-[#00df9a]'>REACT</h1>
+        <ul className='p-4 border-b border-gray-600'>
+        <li className='p-4 border-b border-gray-600'>HOME</li>
+        <li className='p-4 border-b border-gray-600'>COMPANY</li>
+        <li className='p-4 border-b border-gray-600'>RESOURCES</li>
+        <li className='p-4 border-b border-gray-600'>ABOUT</li>
+        <li className='p-4'>CONTACT</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
